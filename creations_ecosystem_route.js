7f13d28d54c77cf67acc2b3ab1a9af3ec698e9a6
Willem van Heemstra <wvanheemstra@icloud.com>
2016-02-17 21:03:17 +0000

    Commit

--- creations_ecosystem_route.js
+++ creations_ecosystem_route.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Route} from 'react-router';
 import App from './components/App';
 import Home from './components/Home';
-
+ 
 export default (
   <Route component={App}>
     <Route path='/' component={Home} />
